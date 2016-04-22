fbf5b52c203f9d02e717bcbfba0259d9fad4185a
Victor QUESNEL <vquesnel@student.42.fr>
2016-04-22 13:23:56 +0200

    gestion mauvais fichiers

--- srcs/get_map.c
+++ srcs/get_map.c
@@ -6,7 +6,7 @@
 /*   By: vquesnel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/07 12:09:44 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/04/21 17:45:02 by vquesnel         ###   ########.fr       */
+/*   Updated: 2016/04/22 13:23:00 by vquesnel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,14 +95,21 @@ t_node			*get_map(int fd, t_mlx *mlx)
 	t_node		*new;
 	int			i;
 	int			y;
+	static int	x;
 
 	i = 0;
 	y = 0;
 	new = NULL;
-	while (get_next_line(fd, &line))
+	if (get_next_line(fd, &line))
 	{
 		new = convert_map(new, line, mlx);
-		free(line);
+		x = ft_tablen(ft_strsplit(line, ' '));
+	}
+	while (get_next_line(fd, &line))
+	{
+		if (ft_tablen(ft_strsplit(line, ' ')) != x)
+			ft_error();
+		new = convert_map(new, line, mlx);
 	}
 	return (new);
 }
