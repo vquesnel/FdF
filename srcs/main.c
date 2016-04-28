ec8b9a86c84f3489b6284c8ab0a77c6df5d15c55
Victor QUESNEL <vquesnel@student.42.fr>
2016-04-28 15:24:20 +0200

    push

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vquesnel <vquesnel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 15:36:57 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/04/28 14:34:13 by vquesnel         ###   ########.fr       */
+/*   Updated: 2016/04/28 15:23:46 by vquesnel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int		fdf(t_env *env)
 {
-	t_proj *tmp;
+	t_proj	*tmp;
+	char	*str;
 
+	str = ft_itoa(env->param->zoom);
 	tmp = env->proj;
 	mlx_clear_window(env->mlx->mlx, env->mlx->win);
 	while (tmp)
@@ -23,6 +25,15 @@ int		fdf(t_env *env)
 		draw_line(env, tmp);
 		tmp = tmp->next;
 	}
+	mlx_string_put(env->mlx->mlx, env->mlx->win, 10, 40, 0x00FF00,\
+			"Current zoom x");
+	mlx_string_put(env->mlx->mlx, env->mlx->win, 150, 40, 0x00FF00, str);
+	if (env->mlx->proj == 1)
+		mlx_string_put(env->mlx->mlx, env->mlx->win, 10, 10, 0xFF0000, \
+				"Current projection is parallel");
+	else if (env->mlx->proj == 0)
+		mlx_string_put(env->mlx->mlx, env->mlx->win, 10, 10, 0x0000FF, \
+				"Current projection is isometrique");
 	return (0);
 }
 
